796dcd2a285ef1824d18ffbec3a5bde76276d1e3
Emmanuel Franquiz <emmanueljfranquizs@gmail.com>
2018-10-03 12:05:10 -0400

    Adding softDelete in jobs methods

--- app/Job.php
+++ app/Job.php
@@ -3,11 +3,18 @@
 namespace App;
 
 use App\Job;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class Job extends Model
 {
+	use SoftDeletes;
+
 	protected $guarded = [];
+
+	protected $date = ['deleted_at'];
+
+	protected $hidden = ['deleted_at'];
 
 
 	public function user(){
--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
     *Para ser tipo date
     */
 
-    protected $date = ['delete_at'];
+    protected $date = ['deleted_at'];
     /**
      * The attributes that are mass assignable.
      *
@@ -33,7 +33,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token','activation_token','active'
+        'password', 'remember_token','activation_token','active','deleted_at'
     ];
 
 
